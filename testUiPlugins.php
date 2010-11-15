8918f9aa3bc936c185a049ea40349394aab6f152
Daniel Saidi <daniel.saidi@gmail.com>
2010-11-15 23:37:00 +0100

    Added new test page for UI plugins and some extra files that will be used by it

---
+++ testUiPlugins.php
@@ -0,0 +1,43 @@
+<?php include "wigbi/wigbi.php"; ?><!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
+	<head>
+		<?php
+			Wigbi::configFile($root . "tests/resources/config.ini");
+			
+			$cleanUp = array_key_exists("clean", $_GET);
+			
+			function addPlugins() {
+				foreach (glob("plugins/data/*.*") as $file)
+					copy($file, "wigbi/plugins/data/" . basename($file));
+				foreach (glob("plugins/ui/*.*") as $file)
+					copy($file, "wigbi/plugins/ui/" . basename($file));
+			}
+			function deletePlugins() {
+				foreach (glob("wigbi/plugins/data/*.*") as $file)
+					unlink($file);
+				foreach (glob("wigbi/plugins/ui/*.*") as $file)
+					unlink($file);
+			} 			
+			
+			if ($cleanUp)
+				deletePlugins();
+			else
+				addPlugins();
+		?>
+	</head>
+	<body>
+		<?php if (!$cleanUp) { ?>
+		
+		
+		<?php } ?>
+		
+		<h1 style="text-align:center">
+			<a href="testUiPlugins.php">Try again</a>  <a href="testUiPlugins.php?clean=1">Finish</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		</h1>
+		<div class="footer">&nbsp;</div>
+		
+	</body>
+</html>
+
+<?php Wigbi::stop(); ?>
