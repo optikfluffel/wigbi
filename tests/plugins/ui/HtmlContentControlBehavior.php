ce7b9b4adc2b7ba66379ed3e516e00d703ee0045
Daniel Saidi <daniel.saidi@gmail.com>
2010-11-15 23:04:51 +0100

    Added a test class stub

---
+++ tests/plugins/ui/HtmlContentControlBehavior.php
@@ -0,0 +1,53 @@
+<?php
+
+class HtmlContentControlBehavior extends UnitTestCase
+{
+	private $htmlContentControl;
+	
+	
+	
+	function HtmlContentControlBehavior()
+	{
+		$this->UnitTestCase("HtmlContentControl UI plugin");
+	}
+	
+	function setUp() { }
+	
+	function tearDown() { }	
+	
+	
+	
+	function test_add_shouldApplyIdToHtml()
+	{
+	}
+	
+	function test_add_shouldAddControlWithObject()
+	{
+	}
+	
+	function test_add_shouldAddControlWithObjectId()
+	{
+	}
+	
+	function test_add_shouldAddControlWithObjectName()
+	{
+	}
+	
+	function test_add_shouldAddControlWithNoObjectInformation()
+	{
+	}
+	
+	function test_add_shouldAddControlAndApplyNameToNewObject()
+	{
+	}
+	
+	function test_add_shouldAddControlWithoutForm()
+	{
+	}
+	
+	function test_add_shouldAddControlWithForm()
+	{
+	}
+}
+
+?>
