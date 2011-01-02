4c39d5aeedf14fce3ea1a3408e6d5e30edd24bcf
Daniel Saidi <daniel.saidi@gmail.com>
2011-01-02 16:42:39 +0100

    Added PHP and PHP.Core tests together since they are much faster now

--- tests/__oldtest_php.php
+++ tests/__oldtest_php.php
@@ -18,6 +18,39 @@
 			cleanUp();
 			if (!$cleanUp)
 				Wigbi::start();
+				
+				
+				
+				
+				
+				 KOPIERA DETTA!!!!!!!!!!!!!!!!
+				 
+				 
+			if (array_key_exists("clean", $_GET))
+			{
+				Wigbi::dbHandler()->query("DROP DATABASE " . Wigbi::dbHandler()->dbName());
+							 
+				foreach (glob("wigbi/plugins/data/*.php") as $file)
+					unlink($file);
+				foreach (glob("wigbi/plugins/ui/*.php") as $file)
+					unlink($file);
+				foreach (glob("wigbi/plugins/ui/*.js") as $file)
+					unlink($file);
+				
+				if (is_file("wigbi/js/wigbi_dataPlugins.js"))
+					unlink("wigbi/js/wigbi_dataPlugins.js");
+				if (is_file("wigbi/js/wigbi_uiPlugins.js"))
+					unlink("wigbi/js/wigbi_uiPlugins.js");
+			}
+			else
+			{
+				include("../resources/tools/SimpleTest/autorun.php");
+			}
+			
+				
+				
+				
+				
 		?>
 		
 		<script type="text/javascript" src="wigbi/bundle/js:tests/resources/jquery.corner.js,tests/resources/test.js"></script>
