9778f9cd60427f59e20c526548fbf2e0538e654d
Daniel Saidi <daniel.saidi@gmail.com>
2012-01-24 21:12:29 +0100

    Adjusted bootstrap and removed unused fake classes

--- wigbi.tests/wigbi.tests.bootstrap.php
+++ wigbi.tests/wigbi.tests.bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 
 require("php/io/FakePhpIncluder.php");
-require("php/ui/FakeJavaScriptIncluder.php");
-require("php/ui/FakeCssIncluder.php");
 
 $wigbi_test_config = array();
 
@@ -16,9 +14,9 @@ $wigbi_test_config["include"]["php"] = "foo_php,bar_php,foobar_php";
 Wigbi::cache("foo");
 Wigbi::configuration(new ArrayBasedConfiguration($wigbi_test_config));
 Wigbi::cookies("foo");
-Wigbi::cssIncluder(new FakeCssIncluder());
+Wigbi::cssIncluder("foo");
 Wigbi::fileSystem("foo");
-Wigbi::jsIncluder(new FakeJavaScriptIncluder());
+Wigbi::jsIncluder("foo");
 Wigbi::logger("foo");
 Wigbi::phpIncluder(new FakePhpIncluder());
 Wigbi::session("foo");
