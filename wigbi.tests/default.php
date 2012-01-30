a9ca74b2db80247f30293f5788b9aba8c37463b2
Daniel Saidi <daniel.saidi@gmail.com>
2012-01-30 22:47:53 +0100

    Ignored cookies

--- wigbi.tests/default.php
+++ wigbi.tests/default.php
@@ -4,7 +4,7 @@ $wigbi_bootstrapper = "wigbi.tests/wigbi.tests.bootstrap.php";
 require("../wigbi/wigbi.php");
 require($wigbi_root . "wigbi.tests/wigbi.tests.include.php");
 
-SimpleTest::ignore('CookieBehavior');
+SimpleTest::ignore('CookiesBehavior');
 SimpleTest::ignore('UrlExistsValidatorgBehavior');
 
 ?>
